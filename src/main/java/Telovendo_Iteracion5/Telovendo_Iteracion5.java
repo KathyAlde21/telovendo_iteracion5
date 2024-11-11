cb69d682e97d74595406b5fa42f59ff3002fa531
Katherine Alderete Gomez <katherinealdereteg@gmail.com>
2024-11-11 00:38:55 -0300

    primer comit

---
+++ src/main/java/Telovendo_Iteracion5/Telovendo_Iteracion5.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package Telovendo_Iteracion5;
+
+/**
+ *
+ * @author Kathy
+ */
+public class Telovendo_Iteracion5 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
